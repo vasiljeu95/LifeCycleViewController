288a5f8f86652bcf81602dbe7e27ae18cd4c6a0e
vasiljeu95 <vasilyeu95@gmail.com>
2019-12-05 22:14:29 +0300

    viewControllerLifeCycleCommit

--- LifeCycleViewController/SecondViewController.swift
+++ LifeCycleViewController/SecondViewController.swift
@@ -10,25 +10,79 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
-    @IBOutlet var openThirdViewController: UIButton!
+    @IBOutlet var openThirdViewController: UIButton! {
+        didSet {
+            print("\(openThirdViewController.currentTitle ?? "")")
+        }
+    }
     
-    var someProperties: String!
+    var someProperties: String! {
+        didSet {
+            print(someProperties ?? "")
+        }
+    }
     
+    // Срабатывает после загрузки View,
+    // вызывается один раз за весь жизненный цикл ViewController-а
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        printMessage()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // Стабатывает перед появлением View на экране
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        printMessage()
     }
-    */
+    
+    // Срабатывает перед тем как размер вью поменяется под экран
+    // например если размеры динамические под размер экрана - настройка тут
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        printMessage()
+    }
+    
+    // Срабатывает после того как размер вью изменился под размер экрана
+    // тут можно сохранять последнее состояние объектаыг
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        printMessage()
+    }
+    
+    // Обновление констрейнтов
+    override func updateViewConstraints() {
+        printMessage()
+        super.updateViewConstraints() // <- вызов именно в конце метода
+    }
+    
+    // Срабатывает после появления вью на экране
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        printMessage()
+    }
+    
+    // Срабатывает при повороте экрана
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        printMessage()
+    }
+    
+    // Срабатывает перед тем, как вью закроется
+    // обычно ставят на паузу выполн. действия,
+    // отменяют ориентацию, стиль статус бара и тд
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        printMessage()
+    }
+    
+    // Срабатывает после закрытия вью
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        printMessage()
+    }
+}
 
+extension UIViewController {
+    func printMessage(function: String = #function) {
+        print("\(title ?? "nil"): \(function)")
+    }
 }
--- LifeCycleViewController/ThirdViewController.swift
+++ LifeCycleViewController/ThirdViewController.swift
@@ -10,21 +10,33 @@ import UIKit
 
 class ThirdViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        printMessage()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        printMessage()
     }
-    */
-
+    
+    // Срабатывает перед тем, как вью закроется
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        printMessage()
+    }
+    
+    // Срабатывает после закрытия вью
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        printMessage()
+    }
+    
+    deinit {
+        printMessage()
+    }
+    
+    @IBAction func closeButton() {
+        dismiss(animated: true)
+    }
 }
